44f21bfd14ad9c06d7c3193c27929bd14bce6015
Stationær\Fisker <pet5631@gmail.com>
2021-11-11 16:35:32 +0100

    Added policies, monte carlo testing and "game()" function
    - Policies added: Greedy and Epsilon-greedy. We have a problem that greedy outperforms e-greedy in most simulations and we dont know why.
    - Game-function takes the stepping-loop from the main and puts it in a function to allow easier monte carlo testing
    
    - Plots now don't work because the position-log is not saved any longer.

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from numpy.random import multivariate_normal as mnormal
 
 # %% Global Variables
-XLIM = 50
-YLIM = 50
+XLIM = 10
+YLIM = 10
 NSTEP = 10000
 RHO = 0.8
+NBEAMS = 4
 
 
 # %% Gridworld Class
@@ -76,7 +77,7 @@ class GridWorld():
 
 # %% Agent Class
 class Agent:
-    def __init__(self, reward, action_space, sigma=1, alpha=0.7 ):
+    def __init__(self, reward, action_space, sigma=1, alpha=0.5, eps=0.3):
         """
         Initialise the agent with the reward matrices. One for each direction.
 
@@ -96,19 +97,20 @@ class Agent:
         self.reward = reward
         self.sigma = sigma
         self.alpha = alpha
+        self.eps = eps
         self.Q = defaultdict(int)
 
     def _get_reward(self, state, action):
-        ida = self.action_space.index(action)
+        mean = self.reward[state[0], state[1], action]
 
-        return (np.sqrt(self.sigma) * np.random.randn(1) +
-                + self.reward[state[0], state[1], ida])
+        # return (np.sqrt(self.sigma) * np.random.randn(1) + self.reward[state[0], state[1], ida])
+        return np.random.normal(mean, self.sigma, 1)
 
-    def get_action(self, state):
+    def greedy(self, state):
         """
         Calculate which action is the most optimum. Currently there is two
         beam direction and it calculate which is the most optimum based
-        on the reward matrix for eact direction.
+        on the reward matrix for each direction.
 
         Parameters
         ----------
@@ -122,17 +124,23 @@ class Agent:
 
         """
         state = tuple(state)
-        r_est = self.Q[state, self.action_space[0]]
         beam_dir = self.action_space[0]
+        r_est = self.Q[state, beam_dir]
 
         for idx, action in enumerate(self.action_space):
             if idx > 0:
                 r_est_new = self.Q[state, action]
                 if r_est_new >= r_est:
-                    r_est = r_est_new
                     beam_dir = action
-
+                    r_est = r_est_new
         return beam_dir
+
+    def e_greedy(self, state):
+        if np.random.random() > self.eps:
+            return self.greedy(state)
+        else:
+            random_action = np.random.choice(self.action_space)
+            return random_action
 
     def update(self, state, action):
         state = tuple(state)
@@ -166,7 +174,7 @@ def getCov(Lx, Ly, rho):
         Has dimension (Lx*Ly, Lx*Ly)
 
     """
-    sigma_squared = 1
+    sigma_squared = 3
 
     pos = np.zeros((Lx * Ly, 2))
     cov = np.zeros((Lx * Ly, Lx * Ly))
@@ -209,16 +217,48 @@ def check_result(reward, Q, beam_space):
 
             Q_choice[x_coord, y_coord] = beam
 
-    result = optimal_choice== Q_choice
+    result = optimal_choice == Q_choice
     return result, optimal_choice, Q_choice
+
+
+def game(agent, reward, environment, step_space, n_steps, policy):
+    # Choose N random steps based on the step space
+    steps = np.random.choice(step_space, n_steps, replace=True)
+
+    # Create position matrix to log path
+    pos_log = np.zeros([len(steps) + 1, 3], dtype=int)
+
+    # Initialise starting point
+    pos_log[0, 0:2] = np.random.randint(0, [XLIM - 1, YLIM - 1], dtype=int)
+
+    if policy == 'e_greedy':
+        # Got N steps
+        for idx, step in enumerate(steps):
+            pos_log[idx, 2] = agent.e_greedy(pos_log[idx, 0:2])
+            pos_log[idx + 1, 0:2] = environment.step(pos_log[idx, 0:2], step)
+            agent.update(pos_log[idx, 0:2], pos_log[idx, 2])
+
+        # Get the last action
+        pos_log[-1, 2] = agent.e_greedy(pos_log[-1, 0:2])
+
+        # return in which positions the q-values find the optimal choice
+        return check_result(reward, agent.Q, agent.action_space)
+    else:
+        # Got N steps
+        for idx, step in enumerate(steps):
+            pos_log[idx, 2] = agent.greedy(pos_log[idx, 0:2])
+            pos_log[idx + 1, 0:2] = environment.step(pos_log[idx, 0:2], step)
+            agent.update(pos_log[idx, 0:2], pos_log[idx, 2])
+
+        # Get the last action
+        pos_log[-1, 2] = agent.greedy(pos_log[-1, 0:2])
+        # return in which positions the q-values find the optimal choice
+        return check_result(reward, agent.Q, agent.action_space)
 
 
 # %% main
 
 if __name__ == '__main__':
-
-    # number of steps:
-    N = NSTEP
 
     # Correlation factor of reward function
     rho = RHO
@@ -228,7 +268,7 @@ if __name__ == '__main__':
                   "left-up", "left-down", "right-up", "right-down"]
 
     # Beam Space
-    beam_space = range(2)
+    beam_space = range(NBEAMS)
 
     # See if cov matrix is avaible:
     if os.path.exists(f"cov/cov_{XLIM}x{YLIM}_{rho}.npy"):
@@ -250,35 +290,28 @@ if __name__ == '__main__':
         reward_tmp = mnormal(mean=np.zeros(XLIM * YLIM), cov=cov, size=1)
         reward[:, :, idx] = reward_tmp.reshape([XLIM, YLIM])
 
+    print('Creating agents')
     # Create agent.
-    agent = Agent(reward, action_space=beam_space)
 
-    # Get enviroment
+    # Get environment
     env = GridWorld()
 
-    # Choose N random steps based on the step space
-    steps = np.random.choice(step_space, N, replace=True)
+    percent_greedy = 0
+    percent_e_greedy = 0
+    NEPISODES = 100
+    for episode in range(NEPISODES):
+        print(episode)
+        agent_greedy = Agent(reward, action_space=beam_space)
+        agent_e_greedy = Agent(reward, action_space=beam_space)
+        greedy_game = game(agent_greedy, reward, env, step_space, NSTEP, 'greedy')
+        percent_greedy += np.count_nonzero(greedy_game[0]) / greedy_game[0].size
 
-    # Create postion matrix to log path
-    pos_log = np.zeros([len(steps) + 1, 3], dtype=int)
+        e_greedy_game = game(agent_e_greedy, reward, env, step_space, NSTEP, 'e_greedy')
+        percent_e_greedy += np.count_nonzero(e_greedy_game[0]) / e_greedy_game[0].size
 
-    # Initialise starting point
-    pos_log[0, 0:2] = np.random.randint(0, [XLIM - 1, YLIM - 1], dtype=int)
+    print(f'We choose the optimal choice {(percent_greedy / NEPISODES) * 100}% of the time with greedy policy')
 
-    # Got N steps
-    for idx, step in enumerate(steps):
-        pos_log[idx, 2] = agent.get_action(pos_log[idx, 0:2])
-        pos_log[idx + 1, 0:2] = env.step(pos_log[idx, 0:2], step)
-        agent.update(pos_log[idx, 0:2], pos_log[idx, 2])
-
-    # Get the last action
-    pos_log[-1, 2] = agent.get_action(pos_log[-1, 0:2])
-
-    # Check in which positions the q-values find the optimal choice
-    resultat = check_result(reward, agent.Q, agent.action_space)[0]
-
-    percent = np.count_nonzero(resultat)/resultat.size
-    print(f'We choose the optimal choice {percent*100}% of the time')
+    print(f'We choose the optimal choice {(percent_e_greedy / NEPISODES) * 100}% of the time with e_greedy policy')
 
     # %% plots
 
